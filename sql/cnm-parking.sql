4be036313c6160b8261ebea3c0775b0281996185
Nick Lopez <nicklopez702@gmail.com>
2015-02-02 09:03:52 -0700

    Modified original schema.  Re-ordered attributes per styling guide and modified gps properties.  https://app.asana.com/0/25630524977600/25630524977685

--- sql/cnm-parking.sql
+++ sql/cnm-parking.sql
@@ -10,9 +10,9 @@ DROP TABLE IF EXISTS visitor;
 -- arbitrary use of character length as 128 for varchar.
 CREATE TABLE admin (
 	adminId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	activation VARCHAR(128),
 	adminEmail VARCHAR(128) NOT NULL,
 	passHash VARCHAR(128),
-	activation VARCHAR(128),
 	salt VARCHAR(128),
 	UNIQUE(adminEmail),
 	PRIMARY KEY(adminId)
@@ -42,24 +42,25 @@ CREATE TABLE visitor (
 CREATE TABLE vehicle (
 	vehicleId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	visitorId INT UNSIGNED NOT NULL,
+	vehicleColor VARCHAR(128) NOT NULL,
+	vehicleMake VARCHAR(128) NOT NULL,
 	vehicleModel VARCHAR(128) NOT NULL,
 	vehiclePlateNumber VARCHAR(128) NOT NULL,
-	vehicleColor VARCHAR(128) NOT NULL,
-	vehicleYear TINYINT(4) NOT NULL,
-	--  4 year specificity
-	vehicleMake VARCHAR(128) NOT NULL,
 	vehiclePlateState VARCHAR(2),
+	vehicleYear TINYINT(4) NOT NULL,
+	INDEX (visitorId),
 	FOREIGN KEY(visitorId) REFERENCES visitor(visitorId),
 	PRIMARY KEY(vehicleId)
 );
 
 CREATE TABLE location (
 	locationId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+-- using pure decimal representation for gps coordinates
+	latitude DOUBLE(4,2) NOT NULL,
 	locationDescription VARCHAR(128) NOT NULL,
 	locationNote VARCHAR(128),
-	latitude DOUBLE(2,2) NOT NULL,
-	longitude DOUBLE(2,2) NOT NULL,
-	-- using pure decimal representation for gps coordinates
+-- using pure decimal representation for gps coordinates
+	longitude DOUBLE(4,2) NOT NULL,
 	PRIMARY KEY(locationId)
 );
 
@@ -78,10 +79,10 @@ CREATE TABLE parkingPass (
 	adminId INT UNSIGNED NOT NULL,
 	parkingSpotId INT UNSIGNED NOT NULL,
 	vehicleId INT UNSIGNED NOT NULL,
-	uuid INT UNSIGNED NOT NULL,
+	endDateTime DATETIME NOT NULL,
 	issuedDateTime DATETIME NOT NULL,
 	startDateTime DATETIME NOT NULL,
-	endDateTime DATETIME NOT NULL,
+	uuid INT UNSIGNED NOT NULL,
 	INDEX(adminId),
 	INDEX(parkingSpotId),
 	INDEX (vehicleId),
