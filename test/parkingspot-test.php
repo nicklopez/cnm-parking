c0c264b275f86ab44e83fc8039a135dc44f7a7ba
Nick Lopez <nicklopez702@gmail.com>
2015-02-06 15:54:28 -0700

    modified parkingspot-test.php.

--- test/parkingspot-test.php
+++ test/parkingspot-test.php
@@ -2,7 +2,6 @@
 // require the encrypted config functions
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
-
 // require the SimpleTest framework <http://www.simpletest.org/>
 // this path is *NOT* universal, but deployed on the bootcamp-coders server
 require_once("/usr/lib/php5/simpletest/autorun.php");
@@ -26,33 +25,20 @@ class ParkingSpotTest extends UnitTestCase {
 	private $mysqli = null;
 
 	/**
-	 * first instance of the object parkingSpot
+	 * instance of the object parkingSpot
 	 */
-	private $parkingSpot1 = null;
-	/**
-	 * second instance of the object parkingSpot
-	 */
-	private $parkingSpot2 = null;
+	private $parkingSpot = null;
 
 	/**
 	 * integer of location id
 	 */
-	private $locationId1 = 17;
-
-	/**
-	 * integer of location id
-	 */
-	private $locationId2 = 17;
+	private $locationId = 1;
 
 	/**
 	 * char(16) of placardNumber
 	 */
-	private $placardNumber1 = "202";
+	private $placardNumber = "203";
 
-	/**
-	 * char(16) of placardNumber
-	 */
-	private $placardNumber2 = "203";
 
 	/**
 	 * sets up the mySQL connection for this test
@@ -66,8 +52,7 @@ class ParkingSpotTest extends UnitTestCase {
 		$this->mysqli = new mysqli($config["hostname"], $config["username"], $config["password"], $config["database"]);
 
 		// second, create an instance of the object under scrutiny
-		$this->parkingSpot1 = new ParkingSpot(null, $this->locationId1, $this->placardNumber2);
-		$this->parkingSpot2 = new ParkingSpot(null, $this->locationId1, $this->placardNumber2);
+		$this->parkingSpot = new ParkingSpot(null, $this->locationId, $this->placardNumber);
 	}
 
 	/**
@@ -75,14 +60,9 @@ class ParkingSpotTest extends UnitTestCase {
 	 **/
 	public function tearDown() {
 		// destroy the object if it was created
-		if($this->parkingSpot1 !== null && $this->parkingSpot1->getParkingSpotId() !== null) {
-			$this->parkingSpot1->delete($this->mysqli);
-			$this->parkingSpot1 = null;
-		}
-
-		if($this->parkingSpot2 !== null && $this->parkingSpot2->getParkingSpotId() !== null) {
-			$this->parkingSpot2->delete($this->mysqli);
-			$this->parkingSpot2 = null;
+		if($this->parkingSpot !== null) {
+			$this->parkingSpot->delete($this->mysqli);
+			$this->parkingSpot = null;
 		}
 
 		//disconnect from mySQL
@@ -96,76 +76,76 @@ class ParkingSpotTest extends UnitTestCase {
 	 */
 	public function testInsertValidParkingSpot() {
 		//zeroth, ensure the parkingSpot and mySQL class are sane
-		$this->assertNotNull($this->parkingSpot1);
+		$this->assertNotNull($this->parkingSpot);
 		$this->assertNotNull($this->mysqli);
 
 		//first, insert the parkingSpot into mySQL
-		$this->parkingSpot1->insert($this->mysqli);
+		$this->parkingSpot->insert($this->mysqli);
 
-		//second, grab an parkingSpot from mySQL
-		$mysqlParkingSpot = ParkingSpot::getParkingSpotByParkingSpotId($this->mysqli, $this->parkingSpot1->getParkingSpotId());
+		//second, grab a parkingSpot from mySQL
+		$mysqlParkingSpot = ParkingSpot::getParkingSpotByParkingSpotId($this->mysqli, $this->parkingSpot->getParkingSpotId());
 
 		//third, assert the parkingSpot created and mySQL's parkingSpot are the same object
-		$this->assertIdentical($this->parkingSpot1->getParkingSpotId(), $mysqlParkingSpot->getParkingSpotId());
-		$this->assertIdentical($this->parkingSpot1->getLocationId(), $mysqlParkingSpot->getLocationId());
-		$this->assertIdentical($this->parkingSpot1->getPlacardNumber(), $mysqlParkingSpot->getPlacardNumber());
+		$this->assertIdentical($this->parkingSpot->getParkingSpotId(), $mysqlParkingSpot->getParkingSpotId());
+		$this->assertIdentical($this->parkingSpot->getLocationId(), $mysqlParkingSpot->getLocationId());
+		$this->assertIdentical($this->parkingSpot->getPlacardNumber(), $mysqlParkingSpot->getPlacardNumber());
 	}
 
 	/**
 	 * test inserting an invalid parkingSpot into mySQL
 	 */
 	public function testInsertInvalidParkingSpot() {
-		//zeroth, ensure the parkingSpot and mySQL class are the same
-		$this->assertNotNull($this->parkingSpot1);
+		//zeroth, ensure the parkingSpot and mySQL class are same
+		$this->assertNotNull($this->parkingSpot);
 		$this->assertNotNull($this->mysqli);
 
 		//first, set the parkingSpot id to an invented value that should never insert in the first place
-		$this->parkingSpot1->setParkingSpotId(666);
+		$this->parkingSpot->setParkingSpotId(999);
 
 		//second, try to insert the parkingSpot and ensure the exception is thrown
 		$this->expectException("mysqli_sql_exception");
-		$this->parkingSpot1->insert($this->mysqli);
+		$this->parkingSpot->insert($this->mysqli);
 
-		//third, set the parkingSpot to null to prevent the tearDown() from deleting an parkingSpot that never existed
-		$this->parkingSpot1 = null;
+		//third, set the parkingSpot to null to prevent the tearDown() from deleting a parkingSpot that never existed
+		$this->parkingSpot = null;
 	}
 
 	/**
-	 * test deleting an parkingSpot from mySQL
+	 * test deleting a parkingSpot from mySQL
 	 */
 	public function testDeleteValidParkingSpot() {
-		//zeroth, ensure the parkingSpot and mySQL class are the sane
-		$this->assertNotNull($this->parkingSpot1);
+		//zeroth, ensure the parkingSpot and mySQL class are sane
+		$this->assertNotNull($this->parkingSpot);
 		$this->assertNotNull($this->mysqli);
 
 		//first, assert the parkingSpot is inserted into mySQL by grabbing it from mySQL and asserting the primary key
-		$this->parkingSpot1->insert($this->mysqli);
-		$mysqlParkingSpot = ParkingSpot::getParkingSpotByParkingSpotId($this->mysqli, $this->parkingSpot1->getParkingSpotId());
-		$this->assertIdentical($this->parkingSpot1->getParkingSpotId(), $mysqlParkingSpot->getParkingSpotId());
+		$this->parkingSpot->insert($this->mysqli);
+		$mysqlParkingSpot = ParkingSpot::getParkingSpotByParkingSpotId($this->mysqli, $this->parkingSpot->getParkingSpotId());
+		$this->assertIdentical($this->parkingSpot->getParkingSpotId(), $mysqlParkingSpot->getParkingSpotId());
 
 		//second, delete the parkingSpot from mySQL and re-grab it from mySQL and assert id does not exist
-		$this->parkingSpot1->delete($this->mysqli);
-		$mysqlParkingSpot= ParkingSpot::getParkingSpotByParkingSpotId($this->mysqli, $this->parkingSpot1->getParkingSpotId());
+		$this->parkingSpot->delete($this->mysqli);
+		$mysqlParkingSpot= ParkingSpot::getParkingSpotByParkingSpotId($this->mysqli, $this->parkingSpot->getParkingSpotId());
 		$this->assertNull($mysqlParkingSpot);
 
 		//third, set the parkingSpot to null to prevent tearDown() from deleting a parkingSpot that has already been deleted
-		$this->parkingSpot1 = null;
+		$this->parkingSpot = null;
 	}
 
 	/**
-	 * test deleting an parkingSpot from mySQL that does not exist
+	 * test deleting a parkingSpot from mySQL that does not exist
 	 */
 	public function testDeleteInvalidParkingSpot() {
-		//zeroth, ensure the parkingSpot and mySQL class are the sane
-		$this->assertNotNull($this->parkingSpot1);
+		//zeroth, ensure the parkingSpot and mySQL class are sane
+		$this->assertNotNull($this->parkingSpot);
 		$this->assertNotNull($this->mysqli);
 
 		//first, try to delete the parkingSpot before inserting it and ensure the exception is thrown
 		$this->expectException('mysqli_sql_exception');
-		$this->parkingSpot1->delete($this->mysqli);
+		$this->parkingSpot->delete($this->mysqli);
 
-		//second, set the parkingSpot to null to prevent tearDown() from deleting an parkingSpot that has already been deleted
-		$this->parkingSpot1 = null;
+		//second, set the parkingSpot to null to prevent tearDown() from deleting a parkingSpot that has already been deleted
+		$this->parkingSpot = null;
 	}
 
 	/**
@@ -173,51 +153,51 @@ class ParkingSpotTest extends UnitTestCase {
 	 */
 	public function testUpdateValidParkingSpot() {
 		//zeroth, ensure the parkingSpot and mySQL class are the sane
-		$this->assertNotNull($this->parkingSpot1);
+		$this->assertNotNull($this->parkingSpot);
 		$this->assertNotNull($this->mysqli);
 
 		//first, assert the parkingSpot is inserted into mySQL by grabbing it from mySQL and asserting the primary key
-		$this->parkingSpot1->insert($this->mysqli);
-		$mysqlParkingSpot = ParkingSpot::getParkingSpotByParkingSpotId($this->mysqli, $this->parkingSpot1->getParkingSpotId());
-		$this->assertIdentical($this->parkingSpot1->getParkingSpotId(), $mysqlParkingSpot->getParkingSpotId());
+		$this->parkingSpot->insert($this->mysqli);
+		$mysqlParkingSpot = ParkingSpot::getParkingSpotByParkingSpotId($this->mysqli, $this->parkingSpot->getParkingSpotId());
+		$this->assertIdentical($this->parkingSpot->getParkingSpotId(), $mysqlParkingSpot->getParkingSpotId());
 
-		//second, grab an parkingSpot from mySQL
-		$mysqlParkingSpot = ParkingSpot::getParkingSpotByParkingSpotId($this->mysqli, $this->parkingSpot1->getParkingSpotId());
+		//second, grab a parkingSpot from mySQL
+		$mysqlParkingSpot = ParkingSpot::getParkingSpotByParkingSpotId($this->mysqli, $this->parkingSpot->getParkingSpotId());
 
 		//third, assert the parkingSpot created and mySQL's parkingSpot are the same object
-		$this->assertIdentical($this->parkingSpot1->getParkingSpotId(), $mysqlParkingSpot->getParkingSpotId());
-		$this->assertIdentical($this->parkingSpot1->getLocationId(), $mysqlParkingSpot->getLocationId());
-		$this->assertIdentical($this->parkingSpot1->getPlacardNumber(), $mysqlParkingSpot->getPlacardNumber());
+		$this->assertIdentical($this->parkingSpot->getParkingSpotId(), $mysqlParkingSpot->getParkingSpotId());
+		$this->assertIdentical($this->parkingSpot->getLocationId(), $mysqlParkingSpot->getLocationId());
+		$this->assertIdentical($this->parkingSpot->getPlacardNumber(), $mysqlParkingSpot->getPlacardNumber());
 	}
 
 
 	/**
-	 * test updating an parkingSpot from mySQL that does not exist
+	 * test updating a parkingSpot from mySQL that does not exist
 	 */
 	public function testUpdateInvalidParkingSpot() {
 		//zeroth, ensure the parkingSpot and mySQL class are the sane
-		$this->assertNotNull($this->parkingSpot1);
+		$this->assertNotNull($this->parkingSpot);
 		$this->assertNotNull($this->mysqli);
 
 		//first, try to update the parkingSpot before inserting it and ensure the exception is thrown
 		$this->expectException("mysqli_sql_exception");
-		$this->parkingSpot1->update($this->mysqli);
+		$this->parkingSpot->update($this->mysqli);
 
 		//second, set the parkingSpot to null to prevent tearDown() from deleting an parkingSpot that has already been deleted
-		$this->parkingSpot1 = null;
+		$this->parkingSpot = null;
 	}
 
 	/**
 	 * test selecting a parking spot by parkingSpotId from mySQL
 	 **/
-	public function testSelectValidVehicleByVehicleId() {
+	public function testSelectValidParkingSpotId() {
 		// zeroth, ensure the parking spot and mySQL class are sane
-		$this->assertNotNull($this->parkingSpot1);
+		$this->assertNotNull($this->parkingSpot);
 		$this->assertNotNull($this->mysqli);
 
 		// first, insert the parking spot into mySQL
-		$this->parkingSpot1->insert($this->mysqli);
-		$mysqlParkingSpot = ParkingSpot::getParkingSpotByParkingSpotId($this->mysqli, $this->parkingSpot1->getParkingSpotId());
+		$this->parkingSpot->insert($this->mysqli);
+		$mysqlParkingSpot = ParkingSpot::getParkingSpotByParkingSpotId($this->mysqli, $this->parkingSpot->getParkingSpotId());
 
 		// second, assert the query returned a result
 		$this->assertNotNull($mysqlParkingSpot);
@@ -226,21 +206,59 @@ class ParkingSpotTest extends UnitTestCase {
 	/**
 	 * test selecting a parking spot that does not exist in mySQL
 	 **/
-	public function testSelectInvalidVehicleByVehicleId() {
-		// zeroth, ensure the vehicle and mySQL class are sane
-		$this->assertNotNull($this->parkingSpot1);
+	public function testSelectInvalidParkingSpotId() {
+		// zeroth, ensure the parking spot and mySQL class are sane
+		$this->assertNotNull($this->parkingSpot);
 		$this->assertNotNull($this->mysqli);
 
-		// first, try to selecting a vehicle by vehicleId
-		$parkingSpot = 0;
+		// first, try to selecting a parking spot by parkingSpotId
+		$parkingSpotId = 0;
 
 		// ensure the exception is thrown
 		$this->expectException("mysqli_sql_exception");
-		$mysqlParkingSpot = ParkingSpot::getParkingSpotByParkingSpotId($this->mysqli, $parkingSpot);
+		$mysqlParkingSpot = ParkingSpot::getParkingSpotByParkingSpotId($this->mysqli, $parkingSpotId);
 
 		// second, assert the query returned no results
 		$this->assertNull($mysqlParkingSpot);
 	}
 
+	/**
+	 * test selecting a parking spot's placard number by location id from mySQL
+	 */
+	public function testSelectValidPlacardNumber() {
+		// zeroth, ensure the placard number and mySQL class are sane
+		$this->assertNotNull($this->parkingSpot);
+		$this->assertNotNull($this->mysqli);
 
+		// first, insert a test placard number into mySQL
+		$this->parkingSpot->insert($this->mysqli);
+		$mysqlParkingSpot = ParkingSpot::getParkingSpotByPlacardNumber($this->mysqli, $this->parkingSpot->getlocationId(), $this->parkingSpot->getPlacardNumber());
+
+		// second, assert the query returned a result
+		$this->assertNotNull($mysqlParkingSpot);
+	}
+
+	/**
+	 * test grabbing a parking spot's placard number by location id from mySQL by non existent content
+	 **/
+	public function testSelectInvalidPlacardNumber() {
+		// zeroth, ensure the placard number and mySQL class are sane
+		$this->assertNotNull($this->parkingSpot);
+		$this->assertNotNull($this->mysqli);
+
+		// first, insert a test placard number
+		$this->parkingSpot->insert($this->mysqli);
+
+		// second, try to grab a placard number from mySQL
+		$locationId = 0;
+		$placardNumber = "NA";
+
+		// ensure the exception is thrown
+		$this->expectException("RangeException");
+		$mysqlParkingSpot = ParkingSpot::getParkingSpotByPlacardNumber($this->mysqli, $locationId, $placardNumber);
+
+		// third, assert the query returned a result
+		$this->assertNull($mysqlParkingSpot);
+	}
 }
+?>
