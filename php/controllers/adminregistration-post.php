92cfbcd6b1b3b5d05ac50ad50d05f3721459043d
David Fevig <dafevig@gmail.com>
2015-02-10 10:52:10 -0700

    admin sign up controller modified https://app.asana.com/0/24973201815950/26466358973473

---
+++ php/controllers/adminregistration-post.php
@@ -0,0 +1,25 @@
+
+<?php
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+require_once("../php/classes/admin.php");
+require_once("../php/classes/adminProfile.php");
+
+// verify the form values have been submitted
+if(@isset($_POST["adminFirstName"]) === false || @isset($_POST["adminLastName"]) === false || @isset($_POST["adminEmail"]) || @isset($_POST["password"])) {
+	echo "<p class=\"alert alert-danger\">form values not complete. verify the form and try again.</p>";
+}
+try {
+	//
+	mysqli_report(MYSQLI_REPORT_STRICT);
+	$configArray = readConfig("/etc/apache2/capstone-mysql/cnmparking.ini");
+	$mysqli = new mysqli($configArray['hostname'], $configArray['username'], $configArray['password'], $configArray['database']);
+	$admin = new Admin(null, "12345678123456781234567812345678", $_POST["adminEmail"], "12345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678", "1234567812345678123456781234567812345678123456781234567812345678");
+	$admin->insert($mysqli);
+	$adminProfile = new AdminProfile(null, $this->adminId->getAdminId(), $_POST["adminFirstName"], $_POST["adminLastName"]);
+	$adminProfile->insert($mysqli);
+
+	echo "<p class=\"alert alert-success\">Admin(id = " . $admin->getAdminId() . ") added!</p>";
+} catch(Exception $exception) {
+	echo "<p class=\"alert alert-danger\">Exception: " . $exception->getMessage() . "</p>";
+}
+?>
