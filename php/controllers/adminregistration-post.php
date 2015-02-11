0c1176bcf28064fd757cbaa47b71ee8c5f501f84
David Fevig <dafevig@gmail.com>
2015-02-11 16:17:52 -0700

    admin registration-post modified

--- php/controllers/adminregistration-post.php
+++ php/controllers/adminregistration-post.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once("adminregistration.php");
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 require_once("../classes/admin.php");
 require_once("../classes/adminprofile.php");
@@ -13,10 +13,18 @@ try {
 	mysqli_report(MYSQLI_REPORT_STRICT);
 	$configArray = readConfig("/etc/apache2/capstone-mysql/cnmparking.ini");
 	$mysqli = new mysqli($configArray['hostname'], $configArray['username'], $configArray['password'], $configArray['database']);
+
 	$admin = new Admin(null, "12345678123456781234567812345678", $_POST["adminEmail"], "12345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678", "1234567812345678123456781234567812345678123456781234567812345678");
 	$admin->insert($mysqli);
-	$adminProfile = new AdminProfile(null, $this->adminId->getAdminId(), $_POST["adminFirstName"], $_POST["adminLastName"]);
+
+	$adminProfile = new AdminProfile(null, $adminId->getAdminId(), $_POST["adminFirstName"], $_POST["adminLastName"]);
 	$adminProfile->insert($mysqli);
+
+// verify the form values have been submitted
+	if(@isset($_POST["adminFirstName"]) === false || @isset($_POST["adminLastName"]) === false || @isset($_POST["adminEmail"]) || @isset($_POST["password"])) {
+		echo "<p class=\"alert alert-danger\">form values not complete. verify the form and try again.</p>";
+	}
+
 
 	echo "<p class=\"alert alert-success\">Admin(id = " . $admin->getAdminId() . ") added!</p>";
 } catch(Exception $exception) {
