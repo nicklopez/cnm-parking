7ea4473d22fee8da576ec185c5d38e109a30a8ae
David Fevig <dafevig@gmail.com>
2015-02-11 22:09:40 -0700

    admin log out controller made https://app.asana.com/0/24973201815950/26466358973477

--- php/controllers/adminlogout.php
+++ php/controllers/adminlogout.php
@@ -1,5 +1,5 @@
 <?php
-// start restart session
+// start admin restart session
 session_start();
 
 // set session to an empty array
@@ -9,7 +9,7 @@ $_SESSION = array();
 $params = session_get_cookie_params();
 setcookie(session_name(), "", 1, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
 
-// destroy the session
+// destroy admin the session
 session_destroy();
 
 // redirect to login page
