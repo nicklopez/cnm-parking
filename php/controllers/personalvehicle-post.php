1d29cea7c8ebf1f22173900621db8ff494122953
David Fevig <dafevig@gmail.com>
2015-02-11 22:06:04 -0700

    personal vehicle controller modified https://app.asana.com/0/24973201815950/26466358973469

---
+++ php/controllers/personalvehicle-post.php
@@ -0,0 +1,28 @@
+<?php
+require_once("personalvehicle.php");
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+require_once("../classes/visitor.php");
+require_once("../classes/vehicle.php");
+
+// verify the form values have been submitted
+if(@isset($_POST["visitorFirstName"]) === false || @isset($_POST["visitorLastName"]) === false || @isset($_POST["visitorEmail"]) === false || @isset($_POST["visitorPhone"]) === false || @isset($_POST["vehicleMake"]) === false || @isset($_POST["vehicleModel"]) === false || @isset($_POST["vehicleYear"]) === false || @isset($_POST["vehicleColor"]) === false || @isset($_POST["vehiclePlateNumber"]) === false || @isset($_POST["vehiclePlateState"]) === false || @isset($_POST["vehiclePlateYear"]) === false) {
+	echo "<p class=\"alert alert-danger\">form values not complete. verify the form and try again.</p>";
+}
+
+try {
+	//
+	mysqli_report(MYSQLI_REPORT_STRICT);
+	$configArray = readConfig("/etc/apache2/capstone-mysql/cnmparking.ini");
+	$mysqli = new mysqli($configArray['hostname'], $configArray['username'], $configArray['password'], $configArray['database']);
+
+	$visitor = new Visitor(null, $_POST["visitorEmail"], $_POST["visitorFirstName"], $_POST["visitorLastName"], $_POST["visitorPhone"]);
+	$visitor->insert($mysqli);
+
+	$vehicle = new Vehicle(null, $visitor->getVisitorId(), $_POST["vehicleColor"], $_POST["vehicleMake"], $_POST["vehicleModel"], $_POST["vehiclePlateNumber"], $_POST["vehiclePlateState"], $_POST["vehicleYear"]);
+	$vehicle->insert($mysqli);
+
+	echo "<p class=\"alert alert-success\">Admin(id = " . $admin->getAdminId() . ") added!</p>";
+} catch(Exception $exception) {
+	echo "<p class=\"alert alert-danger\">Exception: " . $exception->getMessage() . "</p>";
+}
+?>
