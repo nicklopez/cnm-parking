897ffb16449754a4d9cd1f0ba46e10eefe0f9ee3
David Fevig <dafevig@gmail.com>
2015-02-15 17:34:06 -0700

    admin registration post modified https://app.asana.com/0/24973201815950/26466358973473

--- php/controllers/admin-registration-post.php
+++ php/controllers/admin-registration-post.php
@@ -1,30 +1,47 @@
 <?php
+require_once("../../../lib/csrf.php");
 require_once("admin-registration.php");
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 require_once("../classes/admin.php");
 require_once("../classes/adminprofile.php");
 
-
 try {
-	//
+
+	if(@isset($_POST["adminFirstName"]) === false || @isset($_POST["adminLastName"]) === false || @isset($_POST["adminEmail"]) || @isset($_POST["password"])) {
+		echo "<p class=\"alert alert-danger\">form values not complete. verify the form and try again.</p>";
+	}
+
+	// verify the CSRF tokens
+	if(verifyCsrf($_POST["csrfName"], $_POST["csrfToken"]) === false) {
+		throw(new RuntimeException("CSRF tokens incorrect or missing. Make sure cookies are enabled."));
+	}
+
+	// create a new salt and hash
+	$salt = bin2hex(openssl_random_pseudo_bytes(32));
+	$hash = hash_pbkdf2("sha512", $_POST["password"], $salt, 2048, 128);
+
+	// create an admin and admin profile object and insert them into mySQL
 	mysqli_report(MYSQLI_REPORT_STRICT);
 	$configArray = readConfig("/etc/apache2/capstone-mysql/cnmparking.ini");
+	$activation = bin2hex(openssl_random_pseudo_bytes(16));
 	$mysqli = new mysqli($configArray['hostname'], $configArray['username'], $configArray['password'], $configArray['database']);
 
-	$admin = new Admin(null, "12345678123456781234567812345678", $_POST["adminEmail"], "12345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678", "1234567812345678123456781234567812345678123456781234567812345678");
+	$admin = new Admin(null, $activation, $_POST["adminEmail"], $_POST["hash"], $salt);
 	$admin->insert($mysqli);
 
 	$adminProfile = new AdminProfile(null, $admin->getAdminId(), $_POST["adminFirstName"], $_POST["adminLastName"]);
 	$adminProfile->insert($mysqli);
 
-// verify the form values have been submitted
-	if(@isset($_POST["adminFirstName"]) === false || @isset($_POST["adminLastName"]) === false || @isset($_POST["adminEmail"]) || @isset($_POST["password"])) {
-		echo "<p class=\"alert alert-danger\">form values not complete. verify the form and try again.</p>";
+	// email the user with an activation message
+	$to = $user->getEmail();
+	$from = "admin@cnm.edu";
+
+
+
+
+		echo "<p class=\"alert alert-success\">Admin(id = " . $admin->getAdminId() . ") added!</p>";
+	} catch(Exception $exception) {
+		echo "<p class=\"alert alert-danger\">Exception: " . $exception->getMessage() . "</p>";
 	}
 
-
-	echo "<p class=\"alert alert-success\">Admin(id = " . $admin->getAdminId() . ") added!</p>";
-} catch(Exception $exception) {
-	echo "<p class=\"alert alert-danger\">Exception: " . $exception->getMessage() . "</p>";
-}
 ?>
