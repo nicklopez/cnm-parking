b9c3cb2f779e9c3556f2fa8f619d2a91fc0d8911
Kyle Dozier <kedlogic@gmail.com>
2015-02-16 15:25:56 -0700

    Finalized logic https://app.asana.com/0/24973201815917/26466358973479

--- php/controllers/pass-search-by-visitor-name-post.php
+++ php/controllers/pass-search-by-visitor-name-post.php
@@ -1,25 +1,124 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta> charset="UTF-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
+<?php
+/**
+ * controller
+ *
+ * @Author Kyle Dozier <kyle@kedlogic.com>
+ */
 
-		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet" />
+/**
+ * require the encrypted config functions
+ */
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
-		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
-		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/jquery.validate.min.js"></script>
-		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/additional-methods.min.js"></script>
-		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+/**
+ * require the form
+ */
+require_once("pass-search-by-visitor-name.php");
 
-		<title>Search for parking pass by user</title>
-	</head>
-	<body>
-		<h1>Search for parking pass by user</h1>
-		<form method="post" action="pass-search-by-visitor-name-post.php">
-		<label for="fullName">Full Name</label>
-		<input type="text" id="fullName" name="fullname"/><br />
-		<button id="fullNameSubmit" type="submit">Search</button>
-		</form>
-	</body>
-</html>
+/**
+ * verify the form values have been submitted
+ */
+if(@isset($_POST["fullName"]) === false) {
+	echo"<p class=\"alert alert-danger\"> Search field empty. Please insert search criteria and try again.</p>";
+}
+
+try {
+	/**
+	 * connect to mySQL
+	 */
+	mysqli_report(MYSQLI_REPORT_STRICT);
+	$config = readConfig("/etc/apache2/capstone-mysql/cnmparking.ini");
+	$mysqli = new mysqli($config["hostname"], $config["username"], $config["password"], $config["database"]);
+
+	/**
+	 * sanitize input string
+	 */
+	$fullName = filter_var($fullName, FILTER_SANITIZE_STRING);
+	if(empty($fullName) === true) {
+		throw(new InvalidArgumentException("Input contains hostile code"));
+	}
+
+	/**
+	 * split the phrase by any number of commas or spaces, which include " ", \r, \t, \n and \v
+	 */
+	$keywords = preg_split("/[\s,]+/", $_POST["fullName"]);
+	// check if a comma was used
+	$hasComma = strpos($fullName, ',');
+	if($hasComma >= 0) {
+		$hasComma = true;
+	}
+
+	/**
+	 * case 1: assign First and Last Name values from Keywords assuming (First Last)
+	 */
+	if($hasComma === false) {
+		$visitorFirstName = $keywords[0];
+		$visitorLastName  = end($keywords);
+	}
+
+	/**
+	 * case 2: assign First and Last Name values from Keywords assuming (Last, First)
+	 */
+	if($hasComma === true) {
+		$visitorFirstName = $keywords[1];
+		$visitorLastName  = $keywords[0];
+	}
+
+	/**
+	 * preform the search. End result will be $searchResults[]
+	 */
+	$searchResults = array();
+	/**
+	 * if input is only (Name) -> assume (First) -> if null then -> assume (Last)
+	 */
+	if($visitorFirstName === $visitorLastName) {
+		$results = Visitor::getVisitorByVisitorLastName($mysqli, $visitorFirstName);
+		if($results === null) {
+			$results = Visitor::getVisitorByVisitorFirstName($mysqli, $visitorFirstName);
+		}
+		$searchResults = $results;
+	} else {
+		/**
+		 * assume case 1 (First Last)
+		 */
+		$results = Visitor::getVisitorByVisitorLastName($mysqli, $visitorLastName);
+		if($results !== null) {
+			foreach($results as $result) {
+				if(stripos($result->getVisitorFirstName(), $visitorFirstName) >= 0) {
+					$searchResults[] = $result;
+				}
+			}
+		}
+		/**
+		 * if case 1 returns null then assume case 2 (Last, First)
+		 */
+		if(empty($searchResults) === true && $visitorFirstName !== $visitorLastName) {
+			$results = Visitor::getVisitorByVisitorLastName($mysqli, $visitorFirstName);
+			if($results !== null) {
+				foreach($results as $result) {
+					if(stripos($result->getVisitorFirstName(), $visitorLastNameName) >= 0) {
+						$searchResults[] = $result;
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * echo results to html form
+	 */
+	// Preamble
+	echo "<table class='table table-striped table-hover table-responsive'>";
+	echo "<tr><th>First Name</th><th>Last Name</th><th>Email</th><th>Phone</th></tr>";
+	// body
+	foreach($searchResults as $searchResult) {
+		echo "<tr><td>" . $searchResult->getVisitorFirstName() . "</td><td>" . $searchResult->getVisitorLastName() . "</td><td>" . $searchResult->getVisitorEmail() . "</td><td>" . $searchResult->getVisitorPhone() . "</td></tr>";
+	}
+	// postamble
+	echo "</table>";
+
+} catch(mysqli_sql_exception $mysqlException){
+	echo "<p class=\"alert alert-danger\">Exception: " . $exception->getMessage() . "</p>";
+}
+
+?>
--- php/controllers/pass-search-by-visitor-name.php
+++ php/controllers/pass-search-by-visitor-name.php
@@ -1,91 +1,26 @@
-<?php
-/**
- * controller
- *
- * @Author Kyle Dozier <kyle@kedlogic.com>
- */
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta> charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
 
-	/**
-	 * require the encrypted config functions
-	 */
-	require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet" />
 
-	/**
-	 * require the form
-	 */
-	require_once("pass-search-by-visitor-name-post.php");
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/additional-methods.min.js"></script>
+		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 
-	/**
-	 * verify the form values have been submitted
- 	 */
-	if(@isset($_POST["fullName"]) === false) {
-		echo"<p class=\"alert alert-danger\"> Search field empty. Please insert search criteria and try again.</p>";
-	}
-
-	try {
-		/**
-		 * connect to mySQL
-		 */
-		mysqli_report(MYSQLI_REPORT_STRICT);
-		$config = readConfig("/etc/apache2/capstone-mysql/cnmparking.ini");
-		$mysqli = new mysqli($config["hostname"], $config["username"], $config["password"], $config["database"]);
-
-		/**
-		 * sanitize input string
-		 */
-		$fullName = filter_var($fullName, FILTER_SANITIZE_STRING);
-			if(empty($fullName) === true) {
-			throw(new InvalidArgumentException("Input contains hostile code"));
-			}
-
-		/**
-		 * split the phrase by any number of commas or spaces, which include " ", \r, \t, \n and \v
-		 */
-		$keywords = preg_split("/[\s,]+/", $_POST["fullName"]);
-		// check if a comma was used
-		$hasComma = strpos($fullName, ',');
-		if($hasComma >= 0) {
-			$hasComma = true;
-		}
-
-		/**
-		 * assign First and Last Name values from Keywords assuming (First Last)
-		 */
-		if($hasComma === false) {
-			$visitorFirstName = $keywords[0];
-			$visitorLastName  = end($keywords);
-		}
-
-		/**
-		 * assign First and Last Name values from Keywords assuming (Last, First)
-		 */
-		if($hasComma === true) {
-			$visitorFirstName = $keywords[1];
-			$visitorLastName  = $keywords[0];
-		}
-
-
-		$results = Visitor::getVisitorByVisitorLastName($mysqli, $visitorLastName);
-		if($results !== null) {
-			foreach($results as $result) {
-				if(stripos($result->getVisitorFirstName(), $visitorFirstName) >= 0){
-
-				}
-			}
-		}
-		if($results === null) {
-			$results = Visitor::getVisitorByVisitorLastName($mysqli, $visitorFirstName);
-
-		}
-
-
-
-
-
-
-
-
-
-	}
-
-?>
+		<title>Search for parking pass by user</title>
+	</head>
+	<body>
+		<h1>Search for parking pass by user</h1>
+		<form id="fullNameSearchForm" method="post" action="pass-search-by-visitor-name-post.php">
+			<label for="fullName">Full Name</label>
+			<input type="text" id="fullName" name="fullName"/><br />
+			<button id="fullNameSubmit" type="submit">Search</button>
+		</form>
+		<div id="outputArea"></div>
+	</body>
+</html>
