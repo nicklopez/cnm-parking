8223e9796af580e7d95890133b3899391967d2be
Nick Lopez <nicklopez702@gmail.com>
2015-02-17 10:34:33 -0700

    moved request-invite.php into new request-invite directory.

--- request-invite/request-invite.php
+++ request-invite/request-invite.php
@@ -16,12 +16,12 @@
 		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/jquery.validate.min.js"></script>
 		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/additional-methods.min.js"></script>
 		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-		<script type="text/javascript" src="../../js/request-invite.js"></script>
+		<script type="text/javascript" src="../js/request-invite.js"></script>
 		<title>Controller: Request Invite</title>
 	</head>
 	<body>
 		<h1>Controller: Request a parking pass invite</h1>
-		<form id="request-invite" method="post" action="request-invite-post.php" novalidate="novalidate">
+		<form id="request-invite" method="post" action="../php/controllers/request-invite-post.php" novalidate="novalidate">
 			<?php echo generateInputTags(); ?>
 			<label for="emailAddress">Email:</label>
 			<input type="text" id="emailAddress" name="emailAddress" size="128" maxlength="128" placeholder="name@example.com"><br>
