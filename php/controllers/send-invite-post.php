5272214efba0a6c097f00660272f778f422103ea
Nick Lopez <nicklopez702@gmail.com>
2015-02-17 08:27:08 -0700

    modified send-invite.post.  https://app.asana.com/0/24973201815928/26466358973467

--- php/controllers/send-invite-post.php
+++ php/controllers/send-invite-post.php
@@ -1,10 +1,11 @@
 <?php
-// verify the form values have been submitted
-if(isset($_POST["emailAddress"]) === FALSE || empty($_POST["emailAddress"])) {
-	echo "<p class=\"alert alert-danger\">Please enter or verify the email address.</p>";
-}
 
 try {
+	// verify the form values have been submitted
+	if(isset($_POST["emailAddress"]) === FALSE || empty($_POST["emailAddress"])) {
+		throw(new InvalidArgumentException("<p class=\"alert alert-danger\">Invalid or missing email address.</p>"));
+	}
+
 // headers for HTML content
 	$headers = "MIME-Version: 1.0 \r\n";
 	$headers = $headers . "Content-type: text/html; charset=iso-8859-1 \r\n";
