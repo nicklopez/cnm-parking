e02b78b9cb99cc4b564df64abc51615d1ac18941
Nick Lopez <nicklopez702@gmail.com>
2015-02-17 08:13:58 -0700

    committing changes.  send-invite controller still a work in progress.  https://app.asana.com/0/24973201815928/26466358973467

--- php/controllers/send-invite.php
+++ php/controllers/send-invite.php
@@ -1,6 +1,20 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<?php
+		// start a PHP session for CSRF protection
+		session_start();
+
+		// require CSRF protection
+		require_once("../lib/csrf.php");
+
+		// require the encrypted configuration functions
+		require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+		// require the mySQL enabled Invite class
+		require_once("../classes/invite.php");
+
+		?>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet">
@@ -9,14 +23,35 @@
 		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/jquery.validate.min.js"></script>
 		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/additional-methods.min.js"></script>
 		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="../js/request-invite.js"></script>
 		<title>Controller: Send Invite</title>
 	</head>
 	<body>
-		<h1>Controller: Send invite to a visitor</h1>
-		<form method="post" action="send-invite-post.php">
-			<label for="emailAddress">Email:</label>
-			<input type="text" id="emailAddress" name="emailAddress" size="128" maxlength="128" placeholder="name@example.com"><br>
+		<h1>Controller: Send a parking pass invite</h1>
+		<form id="send-invite" method="post" action="send-invite-post.php" novalidate="novalidate">
+			<?php echo generateInputTags();
+
+			try {
+				// now retrieve the configuration parameters
+				$configFile = "/etc/apache2/capstone-mysql/cnmparking.ini";
+				$configArray = readConfig($configFile);
+
+				// first, connect to mysqli
+				mysqli_report(MYSQLI_REPORT_STRICT);
+				$mysqli = new mysqli($configArray["hostname"], $configArray["username"], $configArray["password"], $configArray["database"]);
+
+				// retrieve all pending invites
+				$invites = Invite::getPendingInvite($mysqli);
+				foreach($invites as $invite) {
+					echo "<tr><td>" . $invite->getInviteId() . "</td><br/><td>" . $invite->getActivation() . "</td><br/><td>" . $invite->getVisitorId() . "</td></tr>";
+				}
+				$mysqli->close();
+			} catch(Exception $exception) {
+				echo "<td><tr class=\"alert alert-danger\" colspan=\"4\">Exception: " . $exception->getMessage() . "</td></tr>";
+			}
+			?>
 			<button id="sendInvite" type="submit">Send Invite</button>
 		</form>
+		<p id="outputArea"></p>
 	</body>
 </html>
