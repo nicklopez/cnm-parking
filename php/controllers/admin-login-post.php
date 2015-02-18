3f1480241d45f26e4ba223cea2f8e1ae73fc9ae6
David Fevig <dafevig@gmail.com>
2015-02-18 11:41:23 -0700

    admin login controller modified password query https://app.asana.com/0/24973201815950/26466358973475

--- php/controllers/admin-login-post.php
+++ php/controllers/admin-login-post.php
@@ -11,9 +11,9 @@ $_SESSION["login user"] = $_POST["adminEmail"];
 try {
 	//
 	// verify the form values have been submitted
-	if(@isset($_POST["adminEmail"]) === false || @isset($_POST["password"])) {
-		throw (new InvalidArgumentException("<p class=\"alert alert-danger\">form values not complete. verify the form and try again.</p>"));
-	}
+//	if(@isset($_POST["adminEmail"]) === false || @isset($_POST["password"])) {
+//		throw (new InvalidArgumentException("<p class=\"alert alert-danger\">form values not complete. verify the form and try again.</p>"));
+//	}
 
 	// create a new salt and hash
 	$salt = bin2hex(openssl_random_pseudo_bytes(32));
@@ -24,25 +24,23 @@ try {
 	$configArray = readConfig("/etc/apache2/capstone-mysql/cnmparking.ini");
 	$mysqli = new mysqli($configArray['hostname'], $configArray['username'], $configArray['password'], $configArray['database']);
 
+	// query admin email and hash compare
 	$admin = Admin::getAdminByAdminEmail($mysqli, $_POST["adminEmail"]);
 	if (count($admin) !== 0) {
-		echo "Hello";
+		$hash = hash_pbkdf2("sha512", $_POST["password"], $salt, 2048, 128);
+		if($hash === $admin->getPassHash()) ;
+		echo "Welcome Back!";
+		}
+	else	{
+		echo "incorrect email or password.";
 	}
 
-	$stmt = $mysqli->prepare("SELECT passHash, salt FROM admin WHERE adminEmail = ?");
-	$stmt->bind_param("s", $adminEmail);
-	$stmt->execute();
-	$stmt->bind_result($passHash, $salt);
+	// assign session to logged in admin id
+	$_SESSION["login user"] = $admin->getAdminId();
 
-	$adminPassword = null;
-	$row = $adminPassword->fetch_assoc();
-	if ($row !== null) {
-		$adminPassword = new Admin($row["adminId"], $row["adminEmail"], $row["activation"], $row["passHash"], $row["salt"]);
-		$hash = hash_pbkdf2("sha512", $_POST["password"], $salt, 2048, 128);
-		if($hash === $adminPassword->getPassHash());
-			return true;
-		}
+	print_r($_SESSION);
 
+	// PLACEHOLDER TO REDIRECT TO ADMIN PORTAL
 
 
 			echo "<p class=\"alert alert-success\">Admin(id = " . $admin->getAdminId() . ") logged on!</p>";
