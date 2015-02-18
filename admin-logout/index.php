d909554b5ec68affdd9e297da9c1aa2f6b2c8493
David Fevig <dafevig@gmail.com>
2015-02-18 14:12:14 -0700

    modified logout https://app.asana.com/0/24973201815950/26466358973477

--- admin-logout/index.php
+++ admin-logout/index.php
@@ -2,21 +2,11 @@
 // start admin restart session
 session_start();
 
-// set session to an empty array
-$_SESSION = array();
-
-// destroy the cookie
-$params = session_get_cookie_params();
-setcookie(session_name(), "", 1, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
-
-// unset sesstion
-session_unset();
-
 // destroy admin the session
 session_destroy();
 
 var_dump($_SESSION['login user']);
 
 // redirect to login page
-header("Location: index.php"); // Redirecting To Home Page
+header("Location: /login/index.php"); // Redirecting To Admin Login Page
 ?>
