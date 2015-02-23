3b3ab420f72d43a03b2f91a75e5b0708b29fee72
David Fevig <dafevig@gmail.com>
2015-02-23 14:31:50 -0700

    admin logout modified https://app.asana.com/0/24973201815950/26466358973477

---
+++ test/funcunit/admin-logout-test.js
@@ -0,0 +1,46 @@
+// open a new window with the form under scrutiny
+module("tabs", {
+	setup: function() {
+		F.open("../../admin-login/index.php");
+	}
+});
+
+// global variables for form values
+var VALID_EMAIL = "dafevig@yahoo.com";
+var VALID_PASSWORD = "password";
+
+
+/**
+ * test filling in only valid form data
+ **/
+function testValidFields() {
+	// fill in the form values
+	F("#adminEmail").type(VALID_EMAIL);
+	F("#password").type(VALID_PASSWORD);
+
+	// click the button once all the fields are filled in
+	F("#submit").click();
+
+	// in forms, we want to assert the form worked as expected
+	// here, we assert we got the success message from the AJAX call
+	//F(".alert").visible(function() {
+	//	// create a regular expression that evaluates the successful text
+	//	var successRegex = "Welcome!";
+
+		////// the ok() function from qunit is equivalent to SimpleTest's assertTrue()
+		//ok(F(this).hasClass("alert-success"), "successful alert CSS");
+		//ok(successRegex.test(F(this).html()), "successful message");
+
+		// click logout link
+		expect(0);
+		F("#logout").click();
+
+}
+
+//function testLogout() {
+//	// click logout link
+//	F("#a").click();
+//}
+
+// the test function *MUST* be called in order for the test to execute
+test("test valid fields", testValidFields);
