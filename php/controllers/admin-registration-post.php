07a2daac207b30162670c9fccb8ddbab46b9b093
David Fevig <dafevig@gmail.com>
2015-02-23 11:11:22 -0700

    personal vehicle post modified https://app.asana.com/0/24973201815950/26466358973469

--- php/controllers/admin-registration-post.php
+++ php/controllers/admin-registration-post.php
@@ -33,60 +33,13 @@ try {
 
 	// verify the form was submitted
 	if(@isset($_POST["adminFirstName"]) === false || @isset($_POST["adminLastName"]) === false || @isset($_POST["adminEmail"]) || @isset($_POST["password"])) {
-		throw(new InvalidArgumentException("Please enter or verify the form fields."));
-	}
-	echo "<p class=\"alert alert-success\">Admin(id = " . $admin->getAdminId() . ")added!</p>";
+			throw (new InvalidArgumentException("form values not complete. verify the form and try again."));
 		}
-	catch(Exception $exception) {
-		echo "<p class=\"alert alert-danger\">" . $exception->getMessage() . "</p>";
-
-
-	// email the user with an activation message
-	$to = $admin->getAdminEmail();
-	$from = "noreply@cnm.edu";
-
-	// build headers
-	$headers = array();
-	$headers["To"] = $to;
-	$headers["From"] = $from;
-	$headers["Reply-To"] = $from;
-	$headers["Subject"] = $adminProfile->getAdminFirstName() . " " . $adminProfile->getAdminLastName() . ", Activate your CNM Parking Admin Account";
-	$headers["MIME-Version"] = "1.0";
-	$headers["Content-Type"] = "text/html; charset=UTF-8";
-
-	// build message
-	$pageName = end(explode("/", $_SERVER["PHP_SELF"]));
-	$url = "http://" . $_SERVER["SERVER_NAME"] . $_SERVER["PHP_SELF"];
-	$url = str_replace($pageName, "activate.php", $url);
-	$url = "$url?activation=$activation";
-	$message = <<< EOF
-
-<html>
-	<body>
-		<h1>Welcome New CNM Parking Admin!</h1>
-		<hr />
-		<p>Thank you for registering for a CNM Parking Admin. Visit the following URL to the Admin Login Page <a href="$url">$url</a>.</p>
-	</body>
-</html>
-EOF;
-
-	// send the email
-	error_reporting(E_ALL & ~(E_STRICT|E_NOTICE|E_DEPRECATED));
-	$mailer =& Mail::factory("sendmail");
-	$status = $mailer->send($to, $headers, $message);
-	if(PEAR::isError($status) === true)
-	{
-		echo "<div class=\"alert alert-danger\" role=\"alert\"><strong>Oh snap!</strong> Unable to send mail message:" . $status->getMessage() . "</div>";
+		echo "<p class=\"alert alert-success\">Admin(id = " . $admin->getAdminId() . ")created!</p>";
+		}
+		catch	(Exception $exception) {
+			echo "<p class=\"alert alert-danger\">" . $exception->getMessage() . "</p>";
 	}
-	else
-	{
-		echo "<div class=\"alert alert-success\" role=\"alert\"><strong>Admin registration successful!</strong> Please check your Email to complete the signup process.</div>";
-	}
-
-} catch(Exception $exception) {
-	echo "<div class=\"alert alert-danger\" role=\"alert\"><strong>Oh snap!</strong> Unable to sign up: " . $exception->getMessage() . "</div>";
-}
-
 
 
 ?>
