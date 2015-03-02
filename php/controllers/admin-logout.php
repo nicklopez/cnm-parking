76f6e403de4bac379abe3a95d61a6b2ecabc6c87
David Fevig <dafevig@gmail.com>
2015-03-02 11:24:43 -0700

    logout modified

--- php/controllers/admin-logout.php
+++ php/controllers/admin-logout.php
@@ -1,8 +1,8 @@
 <?php
 	require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+
 	session_start();
 	$_SESSION = array();
-	unset($_SESSION);
 
 	// destroy admin the session
 	session_destroy();
