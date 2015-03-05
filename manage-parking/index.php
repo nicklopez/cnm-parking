d1243ea4af63f1052ffc27d4ea93d4e3742a6186
nicklopez <Welcome2CNM>
2015-03-05 14:20:34 -0700

    created a view and js file for location and parking spot management.  https://app.asana.com/0/24973201815928/28294087886401 https://app.asana.com/0/24973201815928/28294087886403

--- manage-parking/index.php
+++ manage-parking/index.php
@@ -4,36 +4,51 @@ require_once("../php/lib/header.php");
 require_once("../php/classes/location.php");
 require_once("../php/classes/parkingspot.php");
 
+
 // require the encrypted configuration functions
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
 try {
 // now retrieve the configuration parameters
-	$configFile = "/etc/apache2/capstone-mysql/cnmparking.ini";
-	$configArray = readConfig($configFile);
+$configFile = "/etc/apache2/capstone-mysql/cnmparking.ini";
+$configArray = readConfig($configFile);
 
 // first, connect to mysqli
-	mysqli_report(MYSQLI_REPORT_STRICT);
-	$mysqli = new mysqli($configArray["hostname"], $configArray["username"], $configArray["password"], $configArray["database"]);
-	$objects = Location::getAllLocationsAndParkingSpots($mysqli);
-	$locations = $objects["location"];
-	$parkingSpots = $objects["parkingSpot"];
+mysqli_report(MYSQLI_REPORT_STRICT);
+$mysqli = new mysqli($configArray["hostname"], $configArray["username"], $configArray["password"], $configArray["database"]);
 
-	var_dump($locations);
-		//var_dump($parkingSpot);
-
-
-} catch(Exception $exception) {
-	echo "<td><tr class=\"alert alert-danger\" colspan=\"3\">Exception: " . $exception->getMessage() . "</td></tr>";
-}
+$objects = Location::getAllLocationsAndParkingSpots($mysqli);
 
 ?>
-
 <header>
 	<h1>Manage locations & parking spots</h1>
 </header>
 
+<table id="example" class="table-bordered table-responsive table-striped">
+	<thead>
+	<th>Location</th>
+		<th>Placard #</th>
+</thead>
+	<tbody>
+	<?php
 
-<?php
-require_once("../php/lib/footer.php");
-?>
+	foreach($objects as $object) {
+		$locationId = $object["locationId"];
+		$locationNote = $object["locationNote"];
+		$locationDesc = $object["locationDescription"];
+		$placard = $object["placardNumber"];
+		$row = <<< EOF
+		<tr><td></td><td>$placard</td><td>$locationDesc - $locationNote</td></tr>
+EOF;
+			echo $row;
+		}
+	echo "</tbody>";
+	echo "</table>";
+	$mysqli->close();
+
+	} catch(Exception $exception) {
+		echo "<td><tr class=\"alert alert-danger\" colspan=\"3\">Exception: " . $exception->getMessage() . "</td></tr>";
+	}
+
+	require_once("../php/lib/footer.php");
+	?>
