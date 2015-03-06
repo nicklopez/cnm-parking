dd6d10ee1e825b508a4fdd4c340a0828780aa128
nicklopez <Welcome2CNM>
2015-03-06 13:33:34 -0700

    modified code logic for admin login.

--- admin-login/index.php
+++ admin-login/index.php
@@ -1,16 +1,16 @@
 <?php
-$pageTitle = "Admin Login";
-require_once("../php/lib/header.php");
 
+$title = "Admin Login";
+$headerTitle = "CNM Parking Admin Credentials";
+
+require_once("../php/lib/header.php");
 // require CSRF protection
 require_once("../php/lib/csrf.php");
+session_start();
 ?>
 
-	<header>
-		<h1>CNM Parking Admin Credentials </h1>
-	</header>
 	<form id="admin-login" method="post" action="../php/controllers/admin-login-post.php" novalidate="novalidate">
-<!--		--><?php //echo generateInputTags(); ?>
+<?php echo generateInputTags(); ?>
 		<label for="adminEmail">Email:</label>
 		<input type="text" id="adminEmail" name="adminEmail" size="128" maxlength="128" placeholder="name@example.com"><br>
 		<label for="password">Password:</label>
--- php/controllers/admin-login-post.php
+++ php/controllers/admin-login-post.php
@@ -1,68 +1,57 @@
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+
+
 <?php
 // start session
 session_start();
 
+$title = "Admin Portal";
+$headerTitle = "Admin Portal";
+
 // require necessary files
+require_once("../lib/header.php");
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 require_once("../classes/admin.php");
 require_once("../classes/adminprofile.php");
 
+
 // assign the session
 $_SESSION["login user"] = $_POST["adminEmail"];
 
-?>
-	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-	<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
-	<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/jquery.validate.min.js"></script>
-	<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/additional-methods.min.js"></script>
-
-	<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
-	<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-	<script src="../../js/admin-login.js"></script>
-
-<?php
-
-
 try {
-
-	// create a new salt and hash
-	$salt = bin2hex(openssl_random_pseudo_bytes(32));
-
 	// connect to database
 	mysqli_report(MYSQLI_REPORT_STRICT);
 	$configArray = readConfig("/etc/apache2/capstone-mysql/cnmparking.ini");
 	$mysqli = new mysqli($configArray['hostname'], $configArray['username'], $configArray['password'], $configArray['database']);
 
-	// query admin email and hash compare
-	$admin = Admin::getAdminByAdminEmail($mysqli, $_POST["adminEmail"]);
-	if(count($admin) !== 0) {
-		$hash = hash_pbkdf2("sha512", $_POST["password"], $salt, 2048, 128);
-		if($hash === $admin->getPassHash());
 
+	if(@isset($_POST["adminEmail"]) === true && @isset($_POST["password"]) === true) {
+		// query admin email and hash compare
+		$admin = Admin::getAdminByAdminEmail($mysqli, $_POST["adminEmail"]);
+		$hash = hash_pbkdf2("sha512", $_POST["password"], $admin->getSalt(), 2048, 128);
+
+	} else {
+		throw (new InvalidArgumentException("form values not complete. verify the form and try again."));
+	}
+
+	if($hash === $admin->getPassHash()) {
 		// assign session to logged in admin id
 		$adminProfile = AdminProfile::getAdminProfileByAdminId($mysqli, $admin->getAdminId());
-
 		$_SESSION["adminProfile"] = array(
 			'id' => $adminProfile->getAdminProfileId()
 		);
 
 		// session assignment to correct profile id
 		$adminProfileId = $adminProfile->getAdminProfileId();
-		echo '<button id="'.$adminProfileId.'" class="btn btn-default portalButton">Go To Portal</button>';
+		echo '<button id="' . $adminProfileId . '" class="btn btn-primary portalButton">Go To Portal</button>';
+		echo "<p>Click to Continue</p>";
 
 	} else {
 		throw (new mysqli_sql_exception(" incorrect email or password. Try again."));
 	}
+} catch	(Exception $exception) {
+	echo "<p class=\"alert alert-danger\">" . $exception->getMessage() . "</p>";
+}
 
-
-	// verify the form values have been submitted
-	if(@isset($_POST["adminEmail"]) === false || @isset($_POST["password"])=== false) {
-		throw (new InvalidArgumentException("form values not complete. verify the form and try again."));
-	}
-	echo "<p>Click to Continue</p>";
-		}
-	catch	(Exception $exception) {
-		echo "<p class=\"alert alert-danger\">" . $exception->getMessage() . "</p>";
-	}
-
+require_once("../lib/footer.php");
+?>
