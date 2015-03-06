6ccdb547effe0373adbead9de1cbb669c51cee4a
Kyle Dozier <kedlogic@gmail.com>
2015-03-05 19:48:04 -0700

    Working on logic for verifyAvailability https://app.asana.com/0/24973201815917/26466358973481

--- verify-availability/index.php
+++ verify-availability/index.php
@@ -13,7 +13,6 @@
 		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/jquery.validate.min.js"></script>
 		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/additional-methods.min.js"></script>
 		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-
 		<script type="text/javascript" src="../js/datetimepicker.js"></script>
 		<script type="text/javascript" src="../js/verify-availability.js"></script>
 
