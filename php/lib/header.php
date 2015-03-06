72a19bf4dc734dfee60a95a979e0b69317a69122
nicklopez <Welcome2CNM>
2015-03-06 13:32:02 -0700

    modified header.php to include additional js.

--- php/lib/header.php
+++ php/lib/header.php
@@ -16,6 +16,7 @@
 		<script type="text/javascript" src="../js/location-row-grouping.js"></script>
 		<script type="text/javascript" src="../js/reports.js"></script>
 		<script type="text/javascript" src="../../js/reports.js"></script>
+		<script type="text/javascript" src="../../js/admin-login.js"></script>
 
 		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 		<link type="text/css" href="//cdn.datatables.net/1.10.5/css/jquery.dataTables.css" rel="stylesheet">
