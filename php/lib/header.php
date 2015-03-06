249a2128e0896ab7fe4974246be7fb2a2d5570e7
nicklopez <Welcome2CNM>
2015-03-06 10:59:27 -0700

    added custom fonts from web to header file and included new CSS rules.

--- php/lib/header.php
+++ php/lib/header.php
@@ -20,8 +20,19 @@
 		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 		<link type="text/css" href="//cdn.datatables.net/1.10.5/css/jquery.dataTables.css" rel="stylesheet">
 		<link type="text/css" href="../../css/custom.css" rel="stylesheet">
+		<link type="text/css" href="../../fonts/lato.woff" rel="stylesheet">
+		<link type="text/css" href="../../fonts/lato.woff2" rel="stylesheet">
 		<link type="text/css" href="../css/custom.css" rel="stylesheet">
-		<title><?php echo $pageTitle ?></title>
+		<title><?php echo $title ?></title>
 	</head>
+	<div class="headBegin">
+		<h2></h2>
+	</div>
+	<div class="header">
+		<h1><?php echo $headerTitle; ?></h1>
+	</div>
+	<div class="headEnd">
+		<h2></h2>
+	</div>
 		<body>
 			<main>
