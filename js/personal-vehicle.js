6b8a141632228b9ac1b5730f34d3794ade06fa9a
nicklopez <Welcome2CNM>
2015-03-06 14:48:21 -0700

    deleted personal vehicle js file; no longer needed.

--- js/personal-vehicle.js
+++
@@ -1,4 +0,0 @@
-//for(i=0; i<document.index.elements.length; i++) {
-//	document.index.elements[i].disabled=true;
-//}
-//document.getElementById("surroundingarea").style.backgroundColor = "#CCCCCC";
