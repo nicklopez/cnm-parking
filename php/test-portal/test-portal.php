4242505c5dde1205861fda62bb2c635bcdf5f4b0
nicklopez <Welcome2CNM>
2015-03-07 12:43:28 -0700

    modified admin portal navbar.

--- php/test-portal/test-portal.php
+++ php/test-portal/test-portal.php
@@ -6,42 +6,27 @@ require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 require_once("../classes/adminprofile.php");
 require_once("../../php/lib/header.php");
 
-
 mysqli_report(MYSQLI_REPORT_STRICT);
 $configArray = readConfig("/etc/apache2/capstone-mysql/cnmparking.ini");
 $mysqli = new mysqli($configArray['hostname'], $configArray['username'], $configArray['password'], $configArray['database']);
 $adminProfile = AdminProfile::getAdminProfileByAdminProfileId($mysqli, $_SESSION['adminProfileId']);
-
-
 $adminProfileId = $adminProfile->getAdminProfileId();
-
-
-
 ?>
 
-<nav class="navbar navbar-default ">
-	<div id="adminProfile">
-		<p id ="welcome" class="navbar-text pull-right">Welcome back, <?php echo $adminProfile->getAdminFirstName(); ?></p>
-	</div>
-	<div class="container-fluid">
-		<ul class="nav nav-tabs">
-			<li role="presentation" class="active"><a href="#">Home</a></li>
-			<li role="presentation"><a href="#">Create Parking Pass</a></li>
-			<li role="presentation"><a href="#">Manage Parking Spots</a></li>
-			<li role="presentation"><a href="#">Reports</a></li>
-		</ul>
-	</div>
-	<form action="../controllers/admin-logout.php" method="post">
-		    <input id="logout" type="submit" class="btn btn-primary pull-right" value="logout" />
-	</form>
-
-</nav>
-	<body>
-
-
-
+<form action="../controllers/admin-logout.php" method="post">
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<button id="logout" type="submit" class="btn btn-primary navbar-btn pull-right">Log Out</button>
+			<p id ="welcome" class="navbar-text pull-right">Welcome back, <?php echo $adminProfile->getAdminFirstName(); ?></p>
+			<ul class="nav navbar-nav">
+				<a class="active navbar-brand" href="test-portal.php">Home</a>
+				<li role="presentation"><a href="../../personal-vehicle">Create Parking Pass</a></li>
+				<li role="presentation"><a href="../../manage-parking">Manage Parking</a></li>
+				<li role="presentation"><a href="../../reports">Reports</a></li>
+			</ul>
 		</div>
-	</body>
+	</nav>
+</form>
 
 <?php
 require_once("../../php/lib/footer.php");
