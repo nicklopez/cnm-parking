89542563520e0014d0679ebd8de557b1e6ee958f
David Fevig <dafevig@gmail.com>
2015-03-07 15:39:54 -0700

    added date-time css custom

--- php/lib/header.php
+++ php/lib/header.php
@@ -17,7 +17,8 @@
 		<script type="text/javascript" src="../js/reports.js"></script>
 		<script type="text/javascript" src="../../js/reports.js"></script>
 		<script type="text/javascript" src="../../js/admin-login.js"></script>
-		<script type="text/javascript" src="../js/datetimepicker.js"></script>
+<!--		<script type="text/javascript" src="../js/datetimepicker.js"></script>-->
+<!--		<script type="text/javascript" src="../js/verify-availability.js"></script>-->
 
 		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 		<link type="text/css" href="//cdn.datatables.net/1.10.5/css/jquery.dataTables.css" rel="stylesheet">
@@ -25,6 +26,7 @@
 		<link type="text/css" href="../../fonts/lato.woff" rel="stylesheet">
 		<link type="text/css" href="../../fonts/lato.woff2" rel="stylesheet">
 		<link type="text/css" href="../css/custom.css" rel="stylesheet">
+		<link type="text/css" href="../css/datetimepicker.css" rel="stylesheet"/>
 		<title><?php echo $title ?></title>
 	</head>
 	<div class="headBegin">
