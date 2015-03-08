aa84c2ab92c41fafb200b5a0fca68c42739c24ef
David Fevig <dafevig@gmail.com>
2015-03-08 17:12:45 -0600

    header updated with visitor lookup

--- php/lib/header.php
+++ php/lib/header.php
@@ -18,8 +18,9 @@
 		<script type="text/javascript" src="../../js/reports.js"></script>
 		<script type="text/javascript" src="../js/admin-login.js"></script>
 		<script type="text/javascript" src="../../js/admin-login.js"></script>
-<!--		<script type="text/javascript" src="../js/datetimepicker.js"></script>-->
+		<script type="text/javascript" src="../js/datetimepicker.js"></script>
 		<script type="text/javascript" src="../js/verify-availability.js"></script>
+		<script type="text/javascript" src="../js/visitorLookup.js"></script>
 
 		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 		<link type="text/css" href="//cdn.datatables.net/1.10.5/css/jquery.dataTables.css" rel="stylesheet">
