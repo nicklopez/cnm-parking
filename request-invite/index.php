fa082ce63fbdbf3847b4cb119ba8d487944daa89
nicklopez <Welcome2CNM>
2015-03-07 23:11:23 -0700

    added bootstrap.

--- request-invite/index.php
+++ request-invite/index.php
@@ -11,28 +11,31 @@ require_once("../php/lib/csrf.php");
 session_start();
 
 ?>
-	<form id="request-invite" method="post" action="../php/controllers/request-invite-post.php" novalidate="novalidate">
-		<?php echo generateInputTags(); ?>
-		<div class="form-group">
-			<label for="emailAddress">Email:</label>
-			<input type="text" class="form-control email" id="emailAddress" name="emailAddress" size="128" maxlength="128" placeholder="name@example.com">
-		</div>
-		<div class="form-group">
-			<label for="firstName">First Name:</label>
-			<input type="text" class="form-control name" id="firstName" name="firstName" size="128" maxlength="128" placeholder="First Name">
-		</div>
-		<div class="form-group">
-			<label for="lastName">Last Name:</label>
-			<input type="text" class="form-control name" id="lastName" name="lastName" size="128" maxlength="128" placeholder="Last Name">
-		</div>
-		<div class="form-group">
-			<label for="phone">Phone:</label>
-			<input type="text" class="form-control phone" id="phone" name="phone" size="24" maxlength="24" placeholder="Phone Number">
-		</div>
-		<div class="form-group">
-		<button type="submit" id="submitRequest" name="submitRequest" class="btn btn-primary">Send Request</button>
-		</div>
-	</form>
+
+	<div class="container-fluid">
+		<form id="request-invite" method="post" action="../php/controllers/request-invite-post.php" novalidate="novalidate">
+			<?php echo generateInputTags(); ?>
+			<div class="form-group">
+				<label for="emailAddress">Email:</label>
+				<input type="text" class="form-control email" id="emailAddress" name="emailAddress" size="128" maxlength="128" placeholder="name@example.com">
+			</div>
+			<div class="form-group">
+				<label for="firstName">First Name:</label>
+				<input type="text" class="form-control name" id="firstName" name="firstName" size="128" maxlength="128" placeholder="First Name">
+			</div>
+			<div class="form-group">
+				<label for="lastName">Last Name:</label>
+				<input type="text" class="form-control name" id="lastName" name="lastName" size="128" maxlength="128" placeholder="Last Name">
+			</div>
+			<div class="form-group">
+				<label for="phone">Phone:</label>
+				<input type="text" class="form-control phone" id="phone" name="phone" size="24" maxlength="24" placeholder="Phone Number">
+			</div>
+			<div class="form-group">
+				<button type="submit" id="submitRequest" name="submitRequest" class="btn btn-primary">Send Request</button>
+			</div>
+		</form>
+	</div>
 	<p id="outputArea"></p>
 
 	<!--	require the footer file-->
