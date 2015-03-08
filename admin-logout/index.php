0b6c9083d1982127e31abccb3ea229892eddebd4
nicklopez <Welcome2CNM>
2015-03-07 19:39:35 -0700

    added bootstrap code.

--- admin-logout/index.php
+++ admin-logout/index.php
@@ -7,8 +7,9 @@ if(!isset($_SESSION["adminProfileId"])) {
 	echo '<div class="alert alert-success" role="alert" id="message">You have logged out successfully.</div>';
 }
 ?>
-
+	<div class="container-fluid">
 	<a class="btn btn-primary" href="../admin-login/index.php">Return to Log In</a>
+		</div>
 
 <?php
 require_once("../php/lib/footer.php");
