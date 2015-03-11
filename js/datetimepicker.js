ef749acfc509865aca1493bfeb5beb1ed42e5a23
Kyle Dozier <kedlogic@gmail.com>
2015-03-11 10:14:21 -0600

    datetime picker fix - AM/PM now working  (for real this time)  https://app.asana.com/0/24973201815917/26466358973481

--- js/datetimepicker.js
+++ js/datetimepicker.js
@@ -428,8 +428,8 @@
 		value: '',
 		lang: 'en',
 
-		format:	'Y/m/d H:i',
-		formatTime:	'H:i',
+		format:	'Y/m/d g:i a',
+		formatTime:	'g:i a',
 		formatDate:	'Y/m/d',
 
 		startDate:	false, // new Date(), '1986/12/08', '-1970/01/05','-1970/01/05',
