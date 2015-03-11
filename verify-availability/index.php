315f97ecb91bab70bb596b4eea1453711bc5c496
Kyle Dozier <kedlogic@gmail.com>
2015-03-11 10:00:12 -0600

    datetime picker fix(maybe) https://app.asana.com/0/24973201815917/26466358973481

--- verify-availability/index.php
+++ verify-availability/index.php
@@ -1,29 +1,3 @@
-<!--<!DOCTYPE html>-->
-<!--<html>-->
-<!--	<head>-->
-<!--		<meta charset="UTF-8" />-->
-<!--		<meta name="viewport" content="width=device-width, initial-scale=1" />-->
-<!---->
-<!--		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet" />-->
-<!---->
-<!--		<link type="text/css" href="../css/datetimepicker.css" rel="stylesheet" />-->
-<!---->
-<!--		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>-->
-<!--		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>-->
-<!--		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/jquery.validate.min.js"></script>-->
-<!--		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/additional-methods.min.js"></script>-->
-<!--		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>-->
-<!--		<script type="text/javascript" src="../js/datetimepicker.js"></script>-->
-<!--		<script type="text/javascript" src="../js/verify-availability.js"></script>-->
-<!--		<link type="text/css" href="../css/datetimepicker.css" rel="stylesheet" />-->
-<!--		<title>Verify Availability</title>-->
-<!--	</head>-->
-
-<!--//$headerTitle = "Visitor Vehicle Information";-->
-<!--//$title = "CNM Visitor Vehcile Parking Information";-->
-<!--//require_once("../php/lib/header.php");-->
-
-<!--	<body>-->
 		<form id="verifyAvailabilityForm">
 			<!--		<form id="verifyAvailabilityForm" method="post" action="../php/controllers/verify-availability-post.php">-->
 			<div class="container">
@@ -64,5 +38,4 @@
 			</div>
 		</form>
 		<div id="outputArea"></div>
-<!--	</body>-->
 
