5cd1904c52e1a8cdd6bd4af86b9f6163c81a7796
nicklopez <Welcome2CNM>
2015-03-11 10:52:56 -0600

    modified styling.

--- create-pass/index.php
+++ create-pass/index.php
@@ -29,125 +29,135 @@ session_start();
 require_once("../verify-availability/index.php");
 ?>
 
-<div class="container-fluid">
+<div class="container">
+	<hr>
 	<form id="personal-vehicle" method="post" action="../php/controllers/personal-vehicle-post.php">
 		<div class="row">
-			<div class="col-xs-12 col-md-3">
+			<!--			<div class="col-xs-12 col-md-1">-->
+			<div class="form-group">
+				<?php echo generateInputTags(); ?>
+
+				<!--hidden foreign key forms-->
+				<input type="hidden" id="adminProfileId" name="adminProfileId" value=<?php $_SESSION["adminProfileId"] ?>>
+				<input type="hidden" id="visitorId" name="visitorId">
+				<input type="hidden" id="activation" name="activation">
+				<input type="hidden" id="parkingSpotId" name="parkingSpotId">
+
+				<!--				</div>-->
+			</div>
+			<div class="col-xs-12 col-md-6">
 				<div class="form-group">
-					<?php echo generateInputTags(); ?>
-
-					<!--hidden foreign key forms-->
-					<input type="hidden" id="adminProfileId" name="adminProfileId" value="<?php echo $_SESSION["adminProfileId"]; ?>">
-					<input type="hidden" id="visitorId" name="visitorId">
-					<input type="hidden" id="parkingSpotId" name="parkingSpotId">
-					<input type="hidden" id="activation" name="activation">
-
-					<!--					<div class="form-group">-->
-					<label for="visitorEmail">Email: (you can search visitor by email)</label>
+					<label for="visitorEmail">Email:</label>
 					<input type="text" class="form-control email" id="visitorEmail" name="visitorEmail" onchange="showVisitor(this.value);">
-
+				</div>
+				<div class="form-group">
 					<label for="visitorFirstName">First Name:</label>
 					<input type="text" class="form-control name" id="visitorFirstName" name="visitorFirstName">
-
-					<!--					<div class="form-group">-->
+				</div>
+				<div class="form-group">
 					<label for="visitorLastName">Last Name:</label>
-					<input type="text" class="form-control name" id="visitorLastName"  name="visitorLastName">
-
-					<!--					<div class="form-group">-->
+					<input type="text" class="form-control name" id="visitorLastName" name="visitorLastName">
+				</div>
+				<div class="form-group">
 					<label for="visitorPhone">Phone Number:</label>
 					<input type="text" class="form-control phone" id="visitorPhone" name="visitorPhone">
-
 				</div>
 			</div>
-
-			<div class="col-xs-8 col-md-2">
+			<!--			</div>-->
+			<div class="col-xs-12 col-md-6">
 				<div class="form-group">
 					<label for="vehicleYear">Vehicle Year:</label>
 					<input type="text" class="form-control name" id="vehicleYear" name="vehicleYear" size="128" maxlength="128">
-
+				</div>
+				<div class="form-group">
 					<label for="vehicleMake">Vehicle Make:</label>
 					<input type="text" class="form-control name" id="vehicleMake" name="vehicleMake" size="128" maxlength="128">
-
+				</div>
+				<div class="form-group">
 					<label for="vehicleModel">Vehicle Model:</label>
 					<input type="text" class="form-control name" id="vehicleModel" name="vehicleModel" size="128" maxlength="128">
-
+				</div>
+				<div class="form-group">
 					<label for="vehicleColor">Vehicle Color:</label>
 					<input type="text" class="form-control name" id="vehicleColor" name="vehicleColor" size="128" maxlength="128">
-					<div class="form-group">
-						<label for="vehiclePlate">Vehicle Plate #:</label>
-						<input type="text" class="form-control name" id="vehiclePlateNumber" name="vehiclePlateNumber" size="128" maxlength="128">
-						<input hidden="hidden" type="text" id="arrivalDate" name="arrivalDate">
-						<input hidden="hidden" type="text" id="departureDate" name="departureDate">
-					</div>
-					<div class="form-group">
-						<label for="vehiclePlateState">Plate State:</label>
-						<select name="vehiclePlateState" class="btn btn-default" id="vehiclePlateState">
-							<option value="AL">AL</option>
-							<option value="AK">AK</option>
-							<option value="AZ">AZ</option>
-							<option value="AR">AR</option>
-							<option value="CA">CA</option>
-							<option value="CO">CO</option>
-							<option value="CT">CT</option>
-							<option value="DE">DE</option>
-							<option value="FL">FL</option>
-							<option value="GA">GA</option>
-							<option value="HI">HI</option>
-							<option value="ID">ID</option>
-							<option value="IL">IL</option>
-							<option value="IN">IN</option>
-							<option value="IA">IA</option>
-							<option value="KS">KS</option>
-							<option value="KY">KY</option>
-							<option value="LA">LA</option>
-							<option value="ME">ME</option>
-							<option value="MD">MD</option>
-							<option value="MA">MA</option>
-							<option value="MI">MI</option>
-							<option value="MN">MN</option>
-							<option value="MS">MS</option>
-							<option value="MO">MO</option>
-							<option value="MT">MT</option>
-							<option value="NE">NE</option>
-							<option value="NV">NV</option>
-							<option value="NH">NH</option>
-							<option value="NJ">NJ</option>
-							<option selected="selected" value="NM">NM</option>
-							<option value="NY">NY</option>
-							<option value="NC">NC</option>
-							<option value="ND">ND</option>
-							<option value="OH">OH</option>
-							<option value="OK">OK</option>
-							<option value="OR">OR</option>
-							<option value="PA">PA</option>
-							<option value="RI">RI</option>
-							<option value="SC">SC</option>
-							<option value="SD">SD</option>
-							<option value="TN">TN</option>
-							<option value="TX">TX</option>
-							<option value="UT">UT</option>
-							<option value="VT">VT</option>
-							<option value="VA">VA</option>
-							<option value="WA">WA</option>
-							<option value="WV">WV</option>
-							<option value="WI">WI</option>
-							<option value="WY">WY</option>
-						</select>
-					</div>
-					<button id="sendRequest" class="btn btn-primary btn-lg pull-right" type="submit">Send Request</button>
 				</div>
+				<div class="form-group">
+					<label for="vehiclePlate">Vehicle Plate #:</label>
+					<input type="text" class="form-control name" id="vehiclePlateNumber" name="vehiclePlateNumber" size="128" maxlength="128">
+					<input hidden="hidden" type="text" id="arrivalDate" name="arrivalDate">
+					<input hidden="hidden" type="text" id="departureDate" name="departureDate">
 
+				</div>
+				<div class="form-group">
+					<label for="vehiclePlateState">Plate State:</label>
+					<select name="vehiclePlateState" class="btn btn-default" id="vehiclePlateState">
+						<option value="AL">AL</option>
+						<option value="AK">AK</option>
+						<option value="AZ">AZ</option>
+						<option value="AR">AR</option>
+						<option value="CA">CA</option>
+						<option value="CO">CO</option>
+						<option value="CT">CT</option>
+						<option value="DE">DE</option>
+						<option value="FL">FL</option>
+						<option value="GA">GA</option>
+						<option value="HI">HI</option>
+						<option value="ID">ID</option>
+						<option value="IL">IL</option>
+						<option value="IN">IN</option>
+						<option value="IA">IA</option>
+						<option value="KS">KS</option>
+						<option value="KY">KY</option>
+						<option value="LA">LA</option>
+						<option value="ME">ME</option>
+						<option value="MD">MD</option>
+						<option value="MA">MA</option>
+						<option value="MI">MI</option>
+						<option value="MN">MN</option>
+						<option value="MS">MS</option>
+						<option value="MO">MO</option>
+						<option value="MT">MT</option>
+						<option value="NE">NE</option>
+						<option value="NV">NV</option>
+						<option value="NH">NH</option>
+						<option value="NJ">NJ</option>
+						<option selected="selected" value="NM">NM</option>
+						<option value="NY">NY</option>
+						<option value="NC">NC</option>
+						<option value="ND">ND</option>
+						<option value="OH">OH</option>
+						<option value="OK">OK</option>
+						<option value="OR">OR</option>
+						<option value="PA">PA</option>
+						<option value="RI">RI</option>
+						<option value="SC">SC</option>
+						<option value="SD">SD</option>
+						<option value="TN">TN</option>
+						<option value="TX">TX</option>
+						<option value="UT">UT</option>
+						<option value="VT">VT</option>
+						<option value="VA">VA</option>
+						<option value="WA">WA</option>
+						<option value="WV">WV</option>
+						<option value="WI">WI</option>
+						<option value="WY">WY</option>
+					</select><br>
+				</div>
 			</div>
-		</div>
+
+			<div class="row">
+				<div class="col-xs-12">
+					<button id="sendRequest" class="btn btn-primary btn-lg " type="submit">Send Request</button>
+				</div>
+				<!--					</div>-->
+				<!--				</div>-->
+			</div>
 	</form>
 </div>
 <p id="outputArea"></p>
-<?php
-//require_once("../verify-availability/index.php");
-//?>
+
 
 
 <?php
 require_once("../php/lib/footer.php");
 ?>
-
