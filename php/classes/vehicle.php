f02423c265ca156c39945128716758b134fd399f
Nick Lopez <nicklopez702@gmail.com>
2015-04-02 13:43:55 -0600

    replaced mysqli with PDO.

--- php/classes/vehicle.php
+++ php/classes/vehicle.php
@@ -454,65 +454,53 @@ class Vehicle {
 	/**
 	 * gets the vehicle by vehicle Id
 	 *
-	 * @param resource $mysqli pointer to mySQL connection, by reference
+	 * @param PDO $pdo pointer to mySQL connection, by reference
 	 * @param int $vehicleId vehicle to search for by vehicle id
 	 * @return mixed vehicle found or null if not found
 	 * @throws mysqli_sql_exception when mySQL related errors occur
 	 **/
-	public static function getVehicleByVehicleId(&$mysqli, $vehicleId) {
+	public static function getVehicleByVehicleId(PDO &$pdo, $vehicleId) {
 		// handle degenerate cases
-		if(gettype($mysqli) !== "object" || get_class($mysqli) !== "mysqli") {
-			throw(new mysqli_sql_exception("input is not a mysqli object"));
+		if(gettype($pdo) !== "object" || get_class($pdo) !== "PDO") {
+			throw(new PDOException("input is not a PDO object"));
 		}
 
 		// sanitize the vehicleId before searching
 		$vehicleId = filter_var($vehicleId, FILTER_VALIDATE_INT);
 		if($vehicleId === false) {
-			throw(new mysqli_sql_exception("vehicle id is not an integer"));
+			throw(new PDOException("vehicle id is not an integer"));
 		}
 		if($vehicleId <= 0) {
-			throw(new mysqli_sql_exception("vehicle id is not positive"));
+			throw(new PDOException("vehicle id is not positive"));
 		}
 
 		// create query template
-		$query	 = "SELECT vehicleId, visitorId, vehicleColor, vehicleMake, vehicleModel, vehiclePlateNumber, vehiclePlateState, vehicleYear FROM vehicle WHERE vehicleId= ?";
-		$statement = $mysqli->prepare($query);
+		$query	 = "SELECT vehicleId, visitorId, vehicleColor, vehicleMake, vehicleModel, vehiclePlateNumber, vehiclePlateState, vehicleYear FROM vehicle WHERE vehicleId= :vehicleId";
+		$statement = $pdo->prepare($query);
 		if($statement === false) {
-			throw(new mysqli_sql_exception("unable to prepare statement"));
+			throw(new PDOException("unable to prepare statement"));
 		}
 
 		// bind the vehicle id to the place holder in the template
-		$wasClean = $statement->bind_param("i", $vehicleId);
-		if($wasClean === false) {
-			throw(new mysqli_sql_exception("unable to bind parameters"));
-		}
+		$parameters = array("vehicleId" => $vehicleId);
 
 		// execute the statement
-		if($statement->execute() === false) {
-			throw(new mysqli_sql_exception("unable to execute mySQL statement: " . $statement->error));
-		}
-
-		// get result from the SELECT query
-		$result = $statement->get_result();
-		if($result === false) {
-			throw(new mysqli_sql_exception("unable to get result set"));
-		}
+		$statement->execute($parameters);
+		$statement->setFetchMode(PDO::FETCH_ASSOC);
 
 		// grab the vehicle from mySQL
 		try {
-			$vehicle = null;
-			$row   = $result->fetch_assoc();
+			$vehicle = false;
+			$row = $statement->fetch();
 			if($row !== null) {
 				$vehicle = new Vehicle($row["vehicleId"], $row["visitorId"], $row["vehicleColor"], $row["vehicleMake"], $row["vehicleModel"], $row["vehiclePlateNumber"], $row["vehiclePlateState"], $row["vehicleYear"]);
 			}
 		} catch(Exception $exception) {
 			// if the row couldn't be converted, rethrow it
-			throw(new mysqli_sql_exception($exception->getMessage(), 0, $exception));
+			throw(new PDOException($exception->getMessage(), 0, $exception));
 		}
 
-		// free up memory and return the result
-		$result->free();
-		$statement->close();
+		// return the result
 		return($vehicle);
 	}
 
