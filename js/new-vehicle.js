25d4134a998a254d355a7cc55077ece515076c6c
David Fevig <dafevig@gmail.com>
2015-04-03 14:33:26 -0600

    initial commit

---
+++ js/new-vehicle.js
@@ -0,0 +1,8 @@
+
+function test() {
+	if (document.getElementById('selectVehicle').value == 'addVehicle') {
+		document.getElementById('extra').style.display = 'block';
+	} else {
+		document.getElementById('extra').style.display = 'none';
+	}
+}
