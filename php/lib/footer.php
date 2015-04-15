b87bfb23ed1bb21a35778c08df5dc7761a059e88
David Fevig <dafevig@gmail.com>
2015-04-14 20:29:25 -0600

    changed to "row-fluid"

--- php/lib/footer.php
+++ php/lib/footer.php
@@ -1,6 +1,16 @@
 </main>
-<footer>Made by CNM Parking Team &copy; <?php echo date("Y") ?></footer>
-</body>
+
+<div id=footpad class="navbar-fixed-bottom">
+</div>
+<!---->
+<!--<footer>Made by CNM Parking Team &copy; --><?php //echo date("Y") ?><!--</footer>-->
+<!--</div>-->
+<!--</body>-->
+<div class="row-fluid">
+	<div class="headBegin">
+		<footer>Made by CNM Parking Team &copy; <?php echo date("Y") ?></footer>
+	</div>
+</div>
 <!-- DateTimePicker - this should go after </body> -->
 <link rel="stylesheet" type="text/css" href="//cnmparking.com/css/datetimepicker.css"/ >
 <script src="//cnmparking.com/js/datetimepicker.js"></script>
