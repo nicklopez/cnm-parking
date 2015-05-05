a5f1b802ed5879a8664bf226c1c69712af6a208e
David Fevig <dafevig@gmail.com>
2015-05-05 12:54:03 -0600

    created redirect landing page

---
+++ personal-vehicle/success-redirect.php
@@ -0,0 +1,8 @@
+<?php
+$title = "Parking Pass";
+$headerTitle = "Success!";
+require_once("../php/lib/header.php");
+
+echo "<div class=\"alert alert-success\" role=\"alert\"><strong>Sign up successful!</strong> Please check your email for your parking pass.</div>";
+
+require_once("../php/lib/footer.php");
